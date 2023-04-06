0b86f44119ff1adf17605e2c4dfbc76cac1b5596
Benjamin Wilking <102791996+BenniWi@users.noreply.github.com>
2023-04-06 16:36:44 +0000

    adapt test to new api

--- airodor_wifi_api/tests/test_airodor_wifi_api.py
+++ airodor_wifi_api/tests/test_airodor_wifi_api.py
@@ -29,14 +29,14 @@ class TestAirodorApi(unittest.TestCase):
             ip_addr=ipv4,
             action=airodor.VentilationAction.WRITE_MODE,
             group=airodor.VentilationGroup.A,
-            mode=airodor.VentilationMode.ALTERNATING_MAX,
+            mode=airodor.VentilationModeSet.ALTERNATING_MAX,
         )
         self.assertEqual(result, URL("http://192.168.2.122/msg?Function=WA4"))
         result = airodor.get_request_url(
             ip_addr=ipv4,
             action=airodor.VentilationAction.WRITE_MODE,
             group=airodor.VentilationGroup.A,
-            mode=airodor.VentilationMode.ALTERNATING_MED,
+            mode=airodor.VentilationModeSet.ALTERNATING_MED,
         )
         self.assertEqual(result, URL("http://192.168.2.122/msg?Function=WA2"))
 
@@ -64,12 +64,18 @@ class TestAirodorApi(unittest.TestCase):
         r.text = "RB0"
         a, b = airodor.interpret_answer(r)
         self.assertEqual(a, airodor.VentilationGroup.B)
-        self.assertEqual(b, airodor.VentilationMode.OFF)
+        self.assertEqual(b, airodor.VentilationModeRead.OFF)
 
+        r.text = "RA66"
+        a, b = airodor.interpret_answer(r)
+        self.assertEqual(a, airodor.VentilationGroup.A)
+        self.assertEqual(b, airodor.VentilationModeRead.INSIDE_MAX)
+
+        # test reading a "set" value, might occur right after setting and reading the mode
         r.text = "RA64"
         a, b = airodor.interpret_answer(r)
         self.assertEqual(a, airodor.VentilationGroup.A)
-        self.assertEqual(b, airodor.VentilationMode.INSIDE_MAX)
+        self.assertEqual(b, airodor.VentilationModeSet.INSIDE_MAX)
 
         r.text = "MAOK"
         a, b = airodor.interpret_answer(r)
@@ -87,39 +93,39 @@ class TestAirodorApi(unittest.TestCase):
             # 1.11.2011 11:11:11 -> fourth entry
             datetime(year=2011, month=11, day=1, hour=11, minute=11, second=11),
             airodor.VentilationGroup.A,
-            airodor.VentilationMode.INSIDE_MAX,
+            airodor.VentilationModeSet.INSIDE_MAX,
         )
         ventlist.add_list_item(
             # 1.10.2011 11:11:11 -> third entry
             datetime(year=2011, month=10, day=1, hour=11, minute=11, second=11),
             airodor.VentilationGroup.A,
-            airodor.VentilationMode.ALTERNATING_MAX,
+            airodor.VentilationModeSet.ALTERNATING_MAX,
         )
         ventlist.add_list_item(
             # 1.11.2010 11:11:10 -> first entry
             datetime(year=2010, month=11, day=1, hour=11, minute=11, second=10),
             airodor.VentilationGroup.A,
-            airodor.VentilationMode.ALTERNATING_MED,
+            airodor.VentilationModeSet.ALTERNATING_MED,
         )
         ventlist.add_list_item(
             # 1.11.2010 11:11:11 -> second entry
             datetime(year=2010, month=11, day=1, hour=11, minute=11, second=11),
             airodor.VentilationGroup.A,
-            airodor.VentilationMode.ALTERNATING_MIN,
+            airodor.VentilationModeSet.ALTERNATING_MIN,
         )
 
         # order of VentilationMode should be:
         # ALTERNATING_MED, ALTERNATING_MIN, ALTERNATING_MAX, INSIDE_MAX
-        self.assertEqual(ventlist.timer_list[0].mode, airodor.VentilationMode.ALTERNATING_MED)
-        self.assertEqual(ventlist.timer_list[1].mode, airodor.VentilationMode.ALTERNATING_MIN)
-        self.assertEqual(ventlist.timer_list[2].mode, airodor.VentilationMode.ALTERNATING_MAX)
-        self.assertEqual(ventlist.timer_list[3].mode, airodor.VentilationMode.INSIDE_MAX)
+        self.assertEqual(ventlist.timer_list[0].mode, airodor.VentilationModeSet.ALTERNATING_MED)
+        self.assertEqual(ventlist.timer_list[1].mode, airodor.VentilationModeSet.ALTERNATING_MIN)
+        self.assertEqual(ventlist.timer_list[2].mode, airodor.VentilationModeSet.ALTERNATING_MAX)
+        self.assertEqual(ventlist.timer_list[3].mode, airodor.VentilationModeSet.INSIDE_MAX)
 
         next_item = ventlist.pop_list_item()
-        self.assertEqual(next_item.mode, airodor.VentilationMode.ALTERNATING_MED)
-        self.assertEqual(ventlist.timer_list[0].mode, airodor.VentilationMode.ALTERNATING_MIN)
-        self.assertEqual(ventlist.timer_list[1].mode, airodor.VentilationMode.ALTERNATING_MAX)
-        self.assertEqual(ventlist.timer_list[2].mode, airodor.VentilationMode.INSIDE_MAX)
+        self.assertEqual(next_item.mode, airodor.VentilationModeSet.ALTERNATING_MED)
+        self.assertEqual(ventlist.timer_list[0].mode, airodor.VentilationModeSet.ALTERNATING_MIN)
+        self.assertEqual(ventlist.timer_list[1].mode, airodor.VentilationModeSet.ALTERNATING_MAX)
+        self.assertEqual(ventlist.timer_list[2].mode, airodor.VentilationModeSet.INSIDE_MAX)
 
 
 if __name__ == '__main__':
