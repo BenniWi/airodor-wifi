9550747e00958fe783646976563ffe19b11e198f
BenniWi <102791996+BenniWi@users.noreply.github.com>
2023-04-19 22:55:05 +0200

    change port again

--- run_ventilation_server.sh
+++ run_ventilation_server.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-if lsof -Pi :80 -sTCP:LISTEN -t
+if lsof -Pi :3849 -sTCP:LISTEN -t
 then
-    echo "port 80 already in use, probably the web app is already running" 
+    echo "port 3849 already in use, probably the web app is already running" 
 else
-    echo "starting airodor web app on port 80"
+    echo "starting airodor web app on port 3849"
     SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
     cd $SCRIPT_DIR
     git pull
     cd airodor_web_app
     poetry install
     cd airodor_web_app
-    poetry run flask run --host=0.0.0.0 --port=80
+    poetry run flask run --host=0.0.0.0 --port=3849
 fi
 
